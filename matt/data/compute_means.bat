887ac59476316406afe8444916628f066687d254
Matt <techmatt@gmail.com>
2015-11-25 22:29:49 -0800

    working simulation

--- matt/data/compute_means.bat
+++ matt/data/compute_means.bat
@@ -1,3 +1,3 @@
-compute_image_mean.exe --backend=leveldb circles-train-leveldb circles-mean.binaryproto
+rem compute_image_mean.exe --backend=leveldb circles-train-leveldb circles-mean.binaryproto
 compute_image_mean.exe --backend=leveldb simulation-train-leveldb simulation-mean.binaryproto
 pause
